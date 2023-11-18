3d628e330bc235dca6aaf6d5b8fee7985aca07d3
inspiredbyretardness <shreyas.s666@hotmail.com>
2023-11-18 17:42:43 +0530

    build

--- billApp-UI/src/config/services.js
+++ billApp-UI/src/config/services.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3200/",
+  //baseURL: "http://localhost:3200/",
+  baseURL: "http://3.89.33.230:3200/",
 });
+
